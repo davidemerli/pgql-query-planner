c5bbacb9884fa7cffc1ffb59f2d91bb3699dbced
Davide <davide.merli@outlook.com>
2019-10-30 23:05:54 +0100

    pgx implementation, test graph

--- src/it/polimi/pgql/queryplanner/Main.java
+++ src/it/polimi/pgql/queryplanner/Main.java
@@ -5,13 +5,18 @@ import oracle.pgql.lang.Pgql;
 import oracle.pgql.lang.PgqlException;
 import oracle.pgql.lang.PgqlResult;
 import oracle.pgql.lang.ir.GraphQuery;
+import oracle.pgx.api.*;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
 
-    public static void main(String[] args) throws PgqlException {
+    public static void main(String[] args) throws PgqlException, ExecutionException, InterruptedException {
         QueryPlanner planner = new QueryPlanner();
 
-        try(Pgql pgql = new Pgql()) {
+        try (Pgql pgql = new Pgql()) {
             //Gets the parsed PGQL result from which we extrapolate the GraphQuery
             PgqlResult result = pgql.parse("SELECT n FROM g MATCH (n:Person) -[e:likes]-> (m:Person) WHERE n.name = 'Dave'");
             //Contains the AST structure with the different operators to be executed on the Graph
@@ -19,12 +24,69 @@ public class Main {
 
             QueryPlan plan = planner.generatePlan(graphQuery);
 
-//            System.out.println(planner.getPGQLQueryStringFromPlan(plan));
-
             //TODO: get a new string from the plan tree
-
-            //TODO: execute the plan
         }
+
+        testPGX();
     }
 
+    private static void testPGX() throws PgqlException, ExecutionException, InterruptedException {
+        String example = "SELECT n.name AS from, m.name AS to, n.\"percentuale di figaggine\" AS sottaceti FROM test MATCH (n:Person) -[e:\"IS ENEMY OF\"]- (m:Person) WHERE n.\"percentuale di figaggine\" >= m.\"percentuale di figaggine\"";
+
+        Pgql pgql = new Pgql();
+
+        //Creates the PGX session
+        PgxSession session = makePGXSession("test_abc");
+
+        System.out.println("Loading graph...");
+
+        //Creates and uploads to the server the test graph we are making
+        PgxGraph g = testGraph(session);
+
+        System.out.println("Graph loaded. (with name '" + session.getGraphs().entrySet().stream().findAny().get().getKey() + "')");
+
+        do {
+            System.out.println("Insert PGQL query (write 'quit' to exit): ");
+
+            //Scanning System.in for new PGQL queries to perform on our test graph
+            Scanner scanner = new Scanner(System.in);
+            String query = scanner.nextLine();
+
+            //Exits when requested
+            if (query.equals("quit")) break;
+
+            try {
+                //Parse the query string so we at least know if its syntax is correct
+                PgqlResult res = pgql.parse(query);
+
+                if (res.isQueryValid()) {
+                    //Performs the query and prints the result table
+                    PgqlResultSet r = session.queryPgql(query);
+                    r.print();
+                } else {
+                    System.out.println(res.getErrorMessages());
+                }
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        } while(true);
+
+        //Destroys the graph and the session
+        g.destroy();
+        session.destroy();
+    }
+
+    private static PgxGraph testGraph(PgxSession session) throws ExecutionException, InterruptedException {
+        return GraphMaker.getRandomEnemyGraph(
+                session,
+                "test",
+                new String[]{"Kien", "Dario", "Davide", "Max", "Ari", "Leti", "Dani", "Gio", "Stef", "Giammi", "Luca", "Paso", "Pit"},
+                30);
+    }
+
+
+    public static PgxSession makePGXSession(String name) throws ExecutionException, InterruptedException {
+        ServerInstance si = Pgx.getInstance("http://localhost:7007");
+        return si.createSession(name);
+    }
 }
